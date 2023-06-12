3a8f331fa5ca56bfd1cbc5d40dd1421103927f4a
Mikko <realmiks27@gmail.com>
2023-06-12 16:45:25 -0700

    Style winnerDisplay message

--- script.js
+++ script.js
@@ -1,111 +1,3 @@
-/*const boardContainer = document.querySelector('#board-container');
-
-const gameBoard = (() => {
-    const board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']];
-
-    const getBoard = () => board;
-
-    const displayBoard = () => {
-        let counter = 0;
-        for (let i = 0; i < board.length; i++) {
-            const row = document.createElement('div');
-            for (let j = 0; j < board[i].length; j++) {
-                const cell = document.createElement('div');
-                cell.dataset.number = counter++;
-                cell.textContent = board[i][j];
-                cell.classList.add('cell');
-                row.appendChild(cell);
-            }
-            boardContainer.appendChild(row);
-        }
-    }
-
-    const updateBoard = () => {
-        boardContainer.textContent = ' ';
-        displayBoard();
-    }
-
-    const getCellNumber = () => {
-        //on click, gets that cells dataset.number which will be used in pair with the getCurrentplayer to use in addmarker, 
-        //where add marker will change the board at that index with the corresponding marker
-        const cells = document.querySelectorAll('.cell');
-        cells.forEach(cell => cell.addEventListener('click', e => {
-            return e.target.dataset.number;
-        }));
-    }
-
-    const addMarker = (cellNumber, playerName) => {
-        /*const cells = document.querySelectorAll('.cell');
-
-        cells.forEach(cell => cell.addEventListener('click', e => {
-            //console.log(e.target.dataset.number)
-            if (e.target.textContent != 'X' && e.target.textContent != 'O') {
-                e.target.textContent = player.getMarker();
-
-                // to display updated board array
-                let counter = 0;
-                for (let i = 0; i < board.length; i++) {
-                    for (let j = 0; j < board[i].length; j++) {
-                        if (counter == e.target.dataset.number) {
-                            board[i][j] = player.getMarker();
-                        }
-                        counter++;
-                    }
-                }
-            }
-        }))*/
-       /* let counter = 0;
-        for (let i = 0 ; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                if (counter == cellNumber) {
-                    board[i][j] = playerName.getMarker();
-                    updateBoard();
-                }
-                counter++;
-            }
-        }
-    }
-
-    const isTie = () => {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                if ((board[i][j]) == 'X' || board[i][j] == 'O') {
-                    continue;
-                } else {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    return { getBoard, displayBoard, addMarker, isTie, getCellNumber };
-})();
-
-const player = (marker) => {
-    const getMarker = () => marker;
-
-    return { getMarker };
-};
-
-const gameController = (() => {
-    let playerX = player("X");
-    let playerO = player("O");
-    let activePlayer = playerX;
-    const playRound = () => {
-        const cells = document.querySelectorAll('.cell');
-        cells.forEach(cell => cell.addEventListener('click', e => {
-            gameBoard.addMarker(e.target.dataset.number, activePlayer);
-            activePlayer = activePlayer == playerX ? playerO : playerX;
-        }))
-    }
-
-    return { playRound };
-})();
-
-gameBoard.displayBoard();
-gameController.playRound();*/
-
 const body = document.querySelector('body');
 const winnerDisplay = document.querySelector('#winner-display');
 const nameContainer = document.querySelector('#name-container');
@@ -122,12 +14,12 @@ const player = (marker) => {
 
 const interfaceController = (() => {
     const displayWinner = (winner) => {
-        let message = document.createElement('div');
         if (winner == "Tie") {
             winnerDisplay.textContent = "It's a tie game!";
         } else {
             winnerDisplay.textContent = `Player ${winner} is the winner`;
         }
+       
     };
 
     return { displayWinner };
@@ -242,15 +134,12 @@ const gameController = (() => {
 
     const playRound = () => {
         if (gameBoard.getWinner() == "X") {
-            console.log('player x has won');
             interfaceController.displayWinner("X");
             return;
         } else if (gameBoard.getWinner() == "O") {
-            console.log('player o has won');
             interfaceController.displayWinner("O");
             return;
         } else if (gameBoard.isTie()) {
-            console.log('game is a tie');
             interfaceController.displayWinner("Tie");
             return;
         }
