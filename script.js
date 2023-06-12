6279e10126c19d6129cc0428f1729a5c920453e2
Mikko <realmiks27@gmail.com>
2023-06-12 16:06:49 -0700

    Add board design and navbar

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ form.addEventListener('submit', (e) => {
 */
 
 const gameBoard = (() => {
-    let board = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
+    let board = ['', '', '', '', '', '', '', '', ''];
     let winner;
     const winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [2,4,6], [0,4,8]];
 
@@ -212,7 +212,7 @@ const gameBoard = (() => {
 
     const clearBoard = () => {
         for (let i = 0; i < board.length; i++) {
-            board[i] = '_';
+            board[i] = '';
         }
         resetWinner();
         updateBoard();
